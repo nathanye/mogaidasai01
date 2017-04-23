f8af6cc22d3d287e9a2590e7da426f0960d0c8ef
nathanye <khalil54781@gmail.com>
2017-04-23 21:35:45 +0800

    add admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
 
-create_account = User.create([email: 'milesmao233@gmail.com', password: '111111', password_confirmation: '111111', is_admin: 'true'])
+create_account = User.create([email: 'admin@gmail.com', password: '123456', password_confirmation: '123456', is_admin: 'true'])
 puts "Admin account created."
 
 job_title = [
